ea3a6d3916fcc64ec75286c8a0afbb1f05c74e7a
zwy <zzu_zwy2018@163.com>
2025-05-15 11:35:52 +0800

    终于跑通了

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
@@ -4386,6 +4386,12 @@ bool SelectionDAGISel::mayRaiseFPException(SDNode *N) const {
   // exception.
   if (N->isTargetOpcode()) {
     const SelectionDAGTargetInfo &TSI = CurDAG->getSelectionDAGInfo();
+
+    if (!&TSI) {
+      //// must be return false, modified by zhaowenya
+      return false;
+    }
+
     return TSI.mayRaiseFPException(N->getOpcode());
   }
   return N->isStrictFPOpcode();

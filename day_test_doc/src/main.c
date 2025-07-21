602fb927c0a437cb592861b04110754ba9d41821
zwy <zzu_zwy2018@163.com>
2025-07-21 14:34:04 +0800

    更新ignore文件,day_test_doc下的文件

---
+++ day_test_doc/src/main.c
@@ -0,0 +1,28 @@
+// #include "stdio.h"
+
+#define MAX_SIZE 20
+
+// int a_set[MAX_SIZE];
+
+// int get_element(int index) {
+//     return (MAX_SIZE - index);
+// }
+
+
+int main(){
+
+    int i = MAX_SIZE;
+
+    if(i>15){
+        return 0;
+    }else{
+        return 1;
+    }
+
+    // for (i = 0; i < MAX_SIZE; i++) {
+    //     a_set[i] = get_element(i);
+    // }
+
+    return 0;
+}
+

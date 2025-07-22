846dededd77d4c45ef0fa95222ab1d17346e5d80
zwy <zzu_zwy2018@163.com>
2025-07-22 18:54:34 +0800

    ...

--- llvm/lib/Target/Day/DayTargetMachine.h
+++ llvm/lib/Target/Day/DayTargetMachine.h
@@ -1,16 +1,30 @@
 #ifndef DAYTARGETMACHINE_H_
 #define DAYTARGETMACHINE_H_
 
+#include "llvm/CodeGen/CodeGenTargetMachineImpl.h"
+
 namespace llvm{
-    class DayTargetMachine
-    {
-    private:
-        /* data */
-    public:
-        DayTargetMachine(/* args */);
-        ~DayTargetMachine();
+    class DayTargetMachine : public CodeGenTargetMachineImpl{
+        std::unique_ptr<TargetLoweringObjectFile> TLOF;
+        mutable StringMap<std::unique_ptr<DaySubtarget>> SubtargetMap;
+
+        public:
+            DayTargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                         StringRef FS, const TargetOptions &Options,
+                         std::optional<Reloc::Model> RM,
+                         std::optional<CodeModel::Model> CM, CodeGenOptLevel OL, bool JIT);
+            
+            
+            const DaySubtarget *getSubtargetImpl(const Function &F) const override;
+            // DO NOT IMPLEMENT: There is no such thing as a valid default subtarget,
+            // subtargets are per-function entities based on the target-specific
+            // attributes of each function.
+            const DaySubtarget *getSubtargetImpl() const = delete;
+            TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+            TargetLoweringObjectFile *getObjFileLowering() const override {
+                return TLOF.get();
+            }
     };
-    
     
 }
 

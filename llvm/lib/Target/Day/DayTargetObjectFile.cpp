4b4df2a34dca4ed8e2f7a26647d6455fa0e080e5
zwy <zzu_zwy2018@163.com>
2025-07-27 16:15:28 +0800

    等在添加Day工程文件

--- llvm/lib/Target/Day/DayTargetObjectFile.cpp
+++ llvm/lib/Target/Day/DayTargetObjectFile.cpp
@@ -2,8 +2,5 @@
 #include "DayTargetObjectFile.h"
 #include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
 
-class SparcELFTargetObjectFile : public TargetLoweringObjectFileELF {
-public:
-  SparcELFTargetObjectFile() = default;
 
-};
+

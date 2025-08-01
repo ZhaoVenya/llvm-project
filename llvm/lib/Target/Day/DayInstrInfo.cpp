5fafc3d953b287fb636811e165fa39b4a7e50211
zwy <zzu_zwy2018@163.com>
2025-08-01 16:12:51 +0800

    添加的movxx指令编译过去了

--- llvm/lib/Target/Day/DayInstrInfo.cpp
+++ llvm/lib/Target/Day/DayInstrInfo.cpp
@@ -205,46 +205,30 @@ void DayInstrInfo::copyPhysReg(MachineBasicBlock &MBB,
                                  bool RenamableDest, bool RenamableSrc) const {
 
   const TargetRegisterInfo *TRI = Subtarget.getRegisterInfo();
-  
-  if (Day::GPRRegClass.contains(DestReg, SrcReg)) {
-    BuildMI(MBB, I, DL, get(Day::ADDI), DestReg)
-        .addReg(SrcReg,
-                getKillRegState(KillSrc) | getRenamableRegState(RenamableSrc))
-        .addImm(0);
-    return;
-  }
 
+    if (Day::GPRRegClass.contains(DestReg, SrcReg)) {
+        BuildMI(MBB, I, DL, get(Day::MOVRR), DestReg)
+            .addReg(SrcReg, getKillRegState(KillSrc));
+        return;
+    }
 
-  if (Day::FPRRegClass.contains(DestReg, SrcReg)) {
-    BuildMI(MBB, I, DL, get(Day::PseudoMV_FPR32INX), DestReg)
-        .addReg(SrcReg,
-                getKillRegState(KillSrc) | getRenamableRegState(RenamableSrc));
-    return;
-  }
+    if (Day::FPRRegClass.contains(DestReg, SrcReg)) {
+        BuildMI(MBB, I, DL, get(Day::MOVFF), DestReg)
+            .addReg(SrcReg, getKillRegState(KillSrc));
+        return;
+    }
 
+    if (Day::FPRRegClass.contains(DestReg) && Day::GPRRegClass.contains(SrcReg)) {
+        BuildMI(MBB, I, DL, get(Day::MOVRF), DestReg)
+            .addReg(SrcReg, getKillRegState(KillSrc));
+        return;
+    }
 
-  if (RISCV::FPR32RegClass.contains(DestReg, SrcReg)) {
-    BuildMI(MBB, I, DL, get(RISCV::FSGNJ_S), DestReg)
-        .addReg(SrcReg, getKillRegState(KillSrc))
-        .addReg(SrcReg, getKillRegState(KillSrc));
-    return;
-  }
-
-
-  if (RISCV::FPR32RegClass.contains(DestReg) &&
-      RISCV::GPRRegClass.contains(SrcReg)) {
-    BuildMI(MBB, I, DL, get(RISCV::FMV_W_X), DestReg)
-        .addReg(SrcReg, getKillRegState(KillSrc));
-    return;
-  }
-
-  if (RISCV::GPRRegClass.contains(DestReg) &&
-      RISCV::FPR32RegClass.contains(SrcReg)) {
-    BuildMI(MBB, I, DL, get(RISCV::FMV_X_W), DestReg)
-        .addReg(SrcReg, getKillRegState(KillSrc));
-    return;
-  }
-
+    if (Day::GPRRegClass.contains(DestReg) && Day::FPRRegClass.contains(SrcReg)) {
+        BuildMI(MBB, I, DL, get(Day::MOVFR), DestReg)
+            .addReg(SrcReg, getKillRegState(KillSrc));
+        return;
+    }
 
   const TargetRegisterClass *RegClass =
       TRI->getCommonMinimalPhysRegClass(SrcReg, DestReg);

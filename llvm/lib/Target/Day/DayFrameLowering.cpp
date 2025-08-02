417c10f73f2876b6de4de2f3292adffe5822ae53
Waller Zhao <zzu_zwy2018@163.com>
2025-08-02 21:37:11 +0800

    学习了一下和栈帧相关的知识

--- llvm/lib/Target/Day/DayFrameLowering.cpp
+++ llvm/lib/Target/Day/DayFrameLowering.cpp
@@ -14,13 +14,38 @@ DayFrameLowering::DayFrameLowering(const DaySubtarget &ST)
 
 void DayFrameLowering::emitPrologue(MachineFunction &MF,
                                       MachineBasicBlock &MBB) const {
-                    
+
+    MachineFrameInfo &MFI = MF.getFrameInfo();
+    auto *RVFI = MF.getInfo<DayMachineFunctionInfo>();
+    const DayRegisterInfo *RI = STI.getRegisterInfo();
+    const DayInstrInfo *TII = STI.getInstrInfo();
+    MachineBasicBlock::iterator MBBI = MBB.begin();
+
+
 }
 
 
 void DayFrameLowering::emitEpilogue(MachineFunction &MF,
                                       MachineBasicBlock &MBB) const {
 
+}
+
+
+void DayFrameLowering::determineCalleeSaves(MachineFunction &MF, BitVector &SavedRegs,
+                            RegScavenger *RS = nullptr) const {
+
+}
+
+
+StackOffset DayFrameLowering::getFrameIndexReference(const MachineFunction &MF, int FI,
+                                     Register &FrameReg) const {
+
+}
+
+MachineBasicBlock::iterator DayFrameLowering::eliminateCallFramePseudoInstr(MachineFunction &MF,
+                                MachineBasicBlock &MBB,
+                                MachineBasicBlock::iterator I) const {
+    
 }
 
 
--- llvm/lib/Target/Day/DayFrameLowering.h
+++ llvm/lib/Target/Day/DayFrameLowering.h
@@ -13,10 +13,24 @@ class DayFrameLowering : public TargetFrameLowering {
 public:
   explicit DayFrameLowering(const DaySubtarget &ST);
 
+  // 这个是序言插入实现对应的方法
   void emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
+  // 这个是尾声插入实现对应的方法
   void emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
+  // 这个函数确定哪些寄存器
+  void determineCalleeSaves(MachineFunction &MF, BitVector &SavedRegs,
+                            RegScavenger *RS = nullptr) const override;
+  
+  StackOffset getFrameIndexReference(const MachineFunction &MF, int FI,
+                                     Register &FrameReg) const override;
+  
+  MachineBasicBlock::iterator eliminateCallFramePseudoInstr(MachineFunction &MF,
+                                MachineBasicBlock &MBB,
+                                MachineBasicBlock::iterator I) const override;
 
 protected:
+  // 这个函数返回的是一个bool值，用来表示当前函数时都需要实现帧指针
+  // 帧指针相对于栈指针，基地址比较稳定
   bool hasFPImpl(const MachineFunction &MF) const override;
 
 
